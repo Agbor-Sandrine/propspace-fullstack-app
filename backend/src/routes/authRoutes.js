42a7b296883421081034ace0d3a0f4bd5348e623
opeh sandrine <agborsandrine31@gmail.com>
2026-06-18 02:14:02 +0100

    feat: authentication system with JWT and protected routes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -2,8 +2,15 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 router.post("/login", authController.login);
-
+const authMiddleware = require("../middleware/authMiddleware");
 // REGISTER ROUTE
 router.post("/register", authController.register);
 
 module.exports = router;
+
+router.get("/profile", authMiddleware, (req, res) => {
+  res.json({
+    message: "Protected route accessed",
+    user: req.user
+  });
+});
